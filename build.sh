f4eba0569ee580ee975ffe00038d1d33827b0d82
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2022-03-23 13:02:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 warbudHome="C:/WEBSITES/Warbud"
+folderPath="Builds/Mail"
 
-dotnet publish -c Release -o $warbudHome/Builds/Mail
+rm -r $warbudHome/$folderPath
+dotnet publish -c Release -o $warbudHome/$folderPath
